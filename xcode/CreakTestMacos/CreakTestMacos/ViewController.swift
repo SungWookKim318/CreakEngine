485bdd7b6c0216ec36380dad1ea6ba8e8b19d3ae
김성욱 <SungWook.Kim318@gmail.com>
2023-03-09 15:55:27 +0900

    TestApp 추가

---
+++ xcode/CreakTestMacos/CreakTestMacos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CreakTestMacos
+//
+//  Created by SungWook Kim on 2023/03/09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
